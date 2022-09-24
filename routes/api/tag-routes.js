b7b312a631dd80f773e6d7b93704e67ee1777bb1
CNRBanks <connortamminen@gmail.com>
2022-09-24 09:53:37 -0500

    finished tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,6 +4,12 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
+  try {
+    const tagz = await Tag.findAll({ include: [Product] });
+    res.json(categories);
+  } catch (error) {
+    res.status(400).json(error);
+  }
   // find all tags
   // be sure to include its associated Product data
   // try, await findAll, catch
@@ -12,6 +18,14 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
+  try {
+    const oneTag = await Tag.findOne({ include: [Product],
+    where: { id: req.params.id },
+  });
+  res.json(category);
+  } catch (error) {
+    res.status(400).json(error);
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
   // try, awai findByPk, if - return, catch
@@ -20,16 +34,38 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+  try {
+    const postTag = await Tag.create(req.body);
+    res.json(category);
+  } catch (error) {
+    res.status(400).json(error);
+  }
   // create a new tag
   // add try, await create, and catch
 });
 
 router.put('/:id', (req, res) => {
+  try {
+    const putTag = await Tag.update({
+      where: {id: req.params.id },
+    });
+    res.json(category);
+  } catch (error) {
+    res.status(400).json(error);
+  }
   // update a tag's name by its `id` value
   // add try, await update, if - return, and catch
 });
 
 router.delete('/:id', (req, res) => {
+  try {
+    const deleteTag = await Tag.destroy({
+      where: { id: req.params.id },
+    });
+    res.json(deleteTag);
+  } catch (error) {
+    res.status(400).json(error);
+  }
   // delete on tag by its `id` value
   // add try, await destroy, if - return, and catch
 });
